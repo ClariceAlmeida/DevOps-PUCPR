c61dab8638fda05862a92f77ca91cae1a1e6eee4
Clarice Almeida <almeida.santos.clarice@gmail.com>
2026-04-05 17:19:04 -0300

    atividade formativa semana 3-TPDW

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,53 @@
+import { useState } from 'react'
 import './App.css'
 import Button from './components/Button.tsx'
 
 function App() {
+  const [value, setValue] = useState(0)
+
+  function handleAdd() {
+    setValue(value + 1)
+  }
+
+  function handleSubtract() {
+    setValue(value - 1)
+  }
+
+
   return (
+    // <div className="App">
+    //   <div className="tela">
+    //     0.75
+    //   </div>
+    //   <div className="corpo">
+    //       <Button element='AC' className="span3" />
+    //       <Button element='/' className="laranja" />
+    //       <Button element='7' />
+    //       <Button element='8' />
+    //       <Button element='9' />
+    //       <Button element='*' className="laranja" />
+    //       <Button element='4' />
+    //       <Button element='5' />
+    //       <Button element='6' />
+    //       <Button element='-' className="laranja" />
+    //       <Button element='1' />
+    //       <Button element='2' />
+    //       <Button element='3' />
+    //       <Button element='+' className="laranja"     />
+    //       <Button element='0' className="span2" /> 
+    //       <Button element='.' />
+    //       <Button element='=' className="laranja" />
+    //   </div>
+    // </div>
+
     <div className="App">
       <div className="tela">
-        0.75
+        {value}
       </div>
-      <div className="corpo">
-          <Button element='AC' className="span3" />
-          <Button element='/' className="laranja" />
-          <Button element='7' />
-          <Button element='8' />
-          <Button element='9' />
-          <Button element='*' className="laranja" />
-          <Button element='4' />
-          <Button element='5' />
-          <Button element='6' />
-          <Button element='-' className="laranja" />
-          <Button element='1' />
-          <Button element='2' />
-          <Button element='3' />
-          <Button element='+' className="laranja"     />
-          <Button element='0' className="span2" /> 
-          <Button element='.' />
-          <Button element='=' className="laranja" />
-      </div>
+      <Button element='+ 1' className="span3" onClick={handleAdd} />
+      <Button element='- 1' className="span3" onClick={handleSubtract} />
     </div>
+
   )
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 type ButtonProps = {
     element: string;
     className?: string;
+    onClick?: () => void;
 }
 
-const Button = ({ element, className }: ButtonProps) => {
+const Button = ({ element, className, onClick }: ButtonProps) => {
 
 
   return (
-    <button className={`button ${className}`}>
+    <button className={`button ${className}`} onClick={onClick}>
         {element}
     </button>
   )
